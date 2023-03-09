e02f4755ae70ca3b3fa5e22911114ad0c55b99c2
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-03-09 09:23:02 -0700

    Update credit_card.py

--- credit_card.py
+++ credit_card.py
@@ -3,13 +3,33 @@ def verify(number):
     #determine whether the card number is valid
     digitIndex = []
     count = 0
+    wholeNumber = []
     for digit in number:
         if digit == "-":
             continue
-        digitIndex.insert(count,digit)
+        digitIndex.insert(count, int(digit))
         count += 1
+        digit1 = int(digit)
+        if digit1 == digitIndex[0]:
+            print(f'yes, {digit1}')
+        
+        """wholeNumber = int(digit)
+        digit1 = int(digit)
+        if digit1 == wholeNumber[0]:
+            print(digit1)
+        
+        
+        digitIndex.insert(count,int(digit))
+        count += 1
+    count1 = 0
+    for digit in digitIndex:
+        print(digitIndex[count1])
+        if digit == digitIndex[count1]:
+            
+        
+        
+            print(type(digit), digit)"""
     
-    print(type(digitIndex), digitIndex)
   
   # be sure to indent your code!
   
