60dd3b4ea50b605a45d2b487d5cb8ffa34a5f797
1021114Carlos <36316304+1021114Carlos@users.noreply.github.com>
2023-03-13 18:50:53 -0600

    Update credit_card.py

--- credit_card.py
+++ credit_card.py
@@ -3,24 +3,37 @@ def verify(number):
     #determine whether the card number is valid
     digitIndex = []
     count = 0
-    digitCount = 0
+    digitsCount = 0
     wholeNumber = []
     for digit in number:
         if digit != "-":
             wholeNumber.insert(count, int(digit))
             digitIndex.insert(count, digit)
             count += 1
-            digitCount += int(digit)
+            digitsCount += int(digit)
             
             continue
     total = int(digitIndex[0] + digitIndex[1]) + int(digitIndex[10] + digitIndex[11])
     if wholeNumber[0] == 4:
-        pass
+        rule1 = "pass"
+    else:
+        rule1 = "return 1"
+        return rule1
     if wholeNumber[3] > wholeNumber[4]:
         pass
-    if digitCount//4 == 0:
-        pass
+    if digitsCount//4 == 0:
+        rule3 = "pass rules 1-2"
+    else:
+        rule3 = "return 3"
+        return rule3
     if total == 100:
+        rule4 = "pass rules 1-3"
+        output = rule4
+        return output
+    else:
+        rule4 = "return 4"
+        output = rule4
+        return output
             
         
     
